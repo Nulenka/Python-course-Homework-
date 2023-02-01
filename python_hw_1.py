6d2974bef4d52c88cd0c721016fcf2f1e2b357cd
Nulenka <117688741+Nulenka@users.noreply.github.com>
2023-02-01 21:46:39 +0300

    #8 is added

--- python_hw_1.py
+++ python_hw_1.py
@@ -28,3 +28,15 @@ if a > 99999 and a < 1000000:
         print('Sorry, it is not your day today!')
 else:
     print ('Try again, please!')
+print(" ")
+
+print('Задача 8: Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек, если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника). 3 2 4 -> yes; 3 2 1 -> no.')
+
+n = int(input("Enter n number, please: "))
+m = int(input("Enter m number, please: "))
+k = int(input("Enter k number, please: "))
+if k < n * m and (k % n == 0 or k % m == 0):
+    print("Yes, you can!")
+else:
+    print("No, sorry!")
+print(" ")
